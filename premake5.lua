21e4be6d6174f6295c7504ecda08a99575823297
Milan Dierick <milan.dierick@gmail.com>
2021-01-01 23:47:04 +0100

    Updated project configurations to fix warnings and linker errors

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "On"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,56 +21,53 @@ project "GLFW"
 
     filter "system:linux"
         pic "On"
-
-    systemversion "latest"
-    staticruntime "On"
-
-    files
-    {
-        "src/x11_init.c",
-        "src/x11_monitor.c",
-        "src/x11_window.c",
-        "src/xkb_unicode.c",
-        "src/posix_time.c",
-        "src/posix_thread.c",
-        "src/glx_context.c",
-        "src/egl_context.c",
-        "src/osmesa_context.c",
-        "src/linux_joystick.c"
-    }
-
-    defines
-    {
-        "_GLFW_X11"
-    }
+        systemversion "latest"
+    
+        files
+        {
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/glx_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/linux_joystick.c"
+        }
+    
+        defines
+        {
+            "_GLFW_X11"
+        }
 
     filter "system:windows"
-    systemversion "latest"
-    staticruntime "On"
-
-    files
-    {
-        "src/win32_init.c",
-        "src/win32_joystick.c",
-        "src/win32_monitor.c",
-        "src/win32_time.c",
-        "src/win32_thread.c",
-        "src/win32_window.c",
-        "src/wgl_context.c",
-        "src/egl_context.c",
-        "src/osmesa_context.c"
-    }
-
-    defines
-    {
-        "_GLFW_WIN32",
-        "_CRT_SECURE_NO_WARNINGS"
-    }
+        systemversion "latest"
+    
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c", 
+			"src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+    
+        defines
+        {
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
     
     filter "configurations:Debug"
-    runtime "Debug"
-    symbols "on"
+        runtime "Debug"
+        symbols "on"
 
     filter "configurations:Release"
-    runtime "Release"
-    optimize "on"
+        runtime "Release"
+        optimize "on"
